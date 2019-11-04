2165a5dc95c3bfd4dd9ccc7ec65a68346153b72d
Arli <arli.machado@gmail.com>
2019-11-04 18:13:18 -0300

    configurando eslint e sequelize

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Helloo" });
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Helloo' });
 });
 
 export default routes;
