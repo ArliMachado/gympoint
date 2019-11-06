0ea354011dcaa9391e9b4e2a8e74018dee53f9c6
Arli <arli.machado@gmail.com>
2019-11-06 16:57:56 -0300

    Efetuando validação dos campos de cadastro de alunos

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,6 +6,7 @@ import StudentController from './app/controllers/StudentController';
 
 import validateUserStore from './app/validators/UserStore';
 import validateSessionStore from './app/validators/SessionStore';
+import validateStudentStore from './app/validators/StudentStore';
 
 const routes = new Router();
 
@@ -13,6 +14,6 @@ routes.post('/users', validateUserStore, UserController.store);
 
 routes.post('/sessions', validateSessionStore, SessionController.store);
 
-routes.post('/students', StudentController.store);
+routes.post('/students', validateStudentStore, StudentController.store);
 
 export default routes;
