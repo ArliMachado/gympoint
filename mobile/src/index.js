0409b65d72ef6a4e2dc4456c9ca27ca4db2e3bb5
Arli <arli.machado@gmail.com>
2019-11-16 11:22:24 -0300

    Iniciando projeto mobile

---
+++ mobile/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Text, StatusBar, SafeAreaView} from 'react-native';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="#FFF" />
+
+      <Text>Ola</Text>
+    </>
+  );
+}
