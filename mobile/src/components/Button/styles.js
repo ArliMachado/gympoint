4daead7507ca8166449c2831c5997dd5dbf5678e
Arli <arli.machado@gmail.com>
2019-11-18 12:47:34 -0300

    Refatorando estilo do componente de button

--- mobile/src/components/Button/styles.js
+++ mobile/src/components/Button/styles.js
@@ -1,17 +1,19 @@
 import styled from 'styled-components/native';
 import {RectButton} from 'react-native-gesture-handler';
 
+import {colors, metrics, fonts} from '~/styles';
+
 export const Container = styled(RectButton)`
   height: 45px;
-  background: #ee4e62;
-  border-radius: 4px;
+  background: ${colors.primary};
+  border-radius: ${metrics.baseRadius}px;
 
   align-items: center;
   justify-content: center;
 `;
 
 export const Text = styled.Text`
-  color: #fff;
+  color: ${colors.white};
   font-weight: bold;
-  font-size: 16px;
+  font-size: ${fonts.medium}px;
 `;
