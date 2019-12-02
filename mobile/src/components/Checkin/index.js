e160dd35cc734c91849d622dbdd7417ef8cd4d55
Arli <arli.machado@gmail.com>
2019-12-02 16:08:33 -0300

    formatando a data do checkin

--- mobile/src/components/Checkin/index.js
+++ mobile/src/components/Checkin/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useMemo} from 'react';
+import {parseISO, formatRelative, formatDistanceToNow} from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
 import {Container, Title, CreatedAt} from './styles';
 
 export default function Checkin({data}) {
   const {id, created_at} = data.item;
+
+  const dateParsed = useMemo(() => {
+    return formatDistanceToNow(parseISO(created_at), {
+      locale: pt,
+      addSuffix: true,
+    });
+  }, [created_at]);
+
   return (
     <Container>
       <Title>Check-in #{id}</Title>
-      <CreatedAt>{created_at}</CreatedAt>
+      <CreatedAt>{dateParsed}</CreatedAt>
     </Container>
   );
 }
