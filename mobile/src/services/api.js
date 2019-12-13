af1e4c5484d3d230f2826b51b68a75f48ae7b169
Arli Machado <arli.machado@gmail.com>
2019-12-12 22:53:25 -0300

    Atualizando IP nas configuracoes

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseURL = 'http://192.168.1.103:3333';
-// const baseURL = 'http://127.0.0.1:3333';
+// const baseURL = 'http://192.168.1.103:3333';
+const baseURL = 'http://127.0.0.1:3333';
 
 const api = axios.create({
   baseURL,
